e80dcd1640bcf5704bd1e11b902067297d9ad7e3
parkerduckworth <parkerduckworth@gmail.com>
2018-07-05 16:23:43 -0500

    initial commit/qsort

---
+++ src/qsort.hs
@@ -0,0 +1,7 @@
+-- Quicksort, first-element partitioning.
+
+qsort[]     = []
+qsort(x:xs) = qsort smaller ++ [x] ++ qsort larger
+              where
+                smaller = [a | a <- xs, a <= x]
+                larger = [b | b <- xs, b > x] 
